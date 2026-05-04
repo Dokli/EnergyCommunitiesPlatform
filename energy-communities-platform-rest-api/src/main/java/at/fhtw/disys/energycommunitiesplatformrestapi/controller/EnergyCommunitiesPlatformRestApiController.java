30142f2838491568b65a6045d56b198310a07927
BenjaminStadlmann <wi24b102@technikum-wien.at>
2026-05-04 22:28:29 +0200

    Fehlerhandling in getHistoricData und getCurrentHour eingefügt.

--- energy-communities-platform-rest-api/src/main/java/at/fhtw/disys/energycommunitiesplatformrestapi/controller/EnergyCommunitiesPlatformRestApiController.java
+++ energy-communities-platform-rest-api/src/main/java/at/fhtw/disys/energycommunitiesplatformrestapi/controller/EnergyCommunitiesPlatformRestApiController.java
@@ -3,10 +3,17 @@ package at.fhtw.disys.energycommunitiesplatformrestapi.controller;
 import at.fhtw.disys.energycommunitiesplatformrestapi.service.EnergyCalculationService;
 import at.fhtw.disys.energycommunitiesplatformrestapi.dto.EnergyDataDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.util.List;
+
+
+
 
 @RequiredArgsConstructor
 @RestController
@@ -17,14 +24,27 @@ public class EnergyCommunitiesPlatformRestApiController {
 
     @GetMapping("/current-hour")
     public EnergyDataDto getCurrentHour() {
-        double produced = 120;
-        double consumed = 90;
 
-        return createData(
-                LocalDateTime.now().toString(),
-                produced,
-                consumed
-        );
+        try {
+            Double produced = 120.0;
+            Double consumed = 90.0;
+
+            if (produced == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "produced - no data for current hour");
+            }
+            if (produced == null || consumed == null) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "consumed - no data for current hour");
+            }
+
+
+            return createData(
+                    LocalDateTime.now().toString(),
+                    produced,
+                    consumed
+            );
+        } catch (Exception e) {
+throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+        }
     }
 
     @GetMapping("/historic")
@@ -33,12 +53,25 @@ public class EnergyCommunitiesPlatformRestApiController {
             @RequestParam String to
     ) {
 
-        return List.of(
-                createData("2026-04-21T10:00:00", 80, 100),
-                createData("2026-04-21T11:00:00", 120, 110),
-                createData("2026-04-21T12:00:00", 150, 100),
-                createData("2026-04-21T13:00:00", 90, 130)
-        );
+        try {
+            if (LocalDateTime.parse(from).isAfter(LocalDateTime.parse(to))) {
+                throw new ResponseStatusException((HttpStatus.BAD_REQUEST), "invalid request, from is after to");
+            }
+
+
+            return List.of(
+                    createData("2026-04-21T10:00:00", 80, 100),
+                    createData("2026-04-21T11:00:00", 120, 110),
+                    createData("2026-04-21T12:00:00", 150, 100),
+                    createData("2026-04-21T13:00:00", 90, 130)
+            );
+        }
+     catch (DateTimeException e){
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "dateformat not valid");
+        }
+        catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+        }
     }
 
     private EnergyDataDto createData(String timestamp, double produced, double consumed) {
@@ -47,7 +80,7 @@ public class EnergyCommunitiesPlatformRestApiController {
         double gridImport = calculationService.calculateGridImport(produced, consumed);
         double gridExport = calculationService.calculateGridExport(produced, consumed);
 
-        return new EnergyDataDto(
+        return  new EnergyDataDto(
                 timestamp,
                 produced,
                 consumed,
